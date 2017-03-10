3acd33bd0165f70b5e2427a7740dd6faef0554ac
Todd Johnston <toddjohnston.ibexsys@gmail.com>
2017-03-10 10:21:21 -0800

    Add File

---
+++ src/main/java/com/ibexsys/pwd/model/PasswordUserModel.java
@@ -0,0 +1,49 @@
+package com.ibexsys.pwd.model;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+
+@XmlRootElement(name = "PwdUserModel")
+public class PasswordUserModel {
+
+	// XmLElementWrapper generates a wrapper element around XML representation
+	@XmlElementWrapper(name = "UserSiteList")
+	@XmlElement(name = "Site")
+
+	private ArrayList<Site> siteList;
+	private String pwdFileName;
+	private AppUser appUser;
+
+	public ArrayList<Site> getSitesList() {
+		return siteList;
+	}
+
+	public void setSiteList(ArrayList<Site> pSiteList) {
+		siteList = pSiteList;
+	}
+
+	public String getPwdFileName() {
+		return pwdFileName;
+	}
+
+	public void setPwdFileName(String pPwdFileName) {
+		pwdFileName = pPwdFileName;
+	}
+
+	public AppUser getAppUser() {
+		return appUser;
+	}
+
+	public void setAppUser(AppUser pAppUser) {
+		appUser = pAppUser;
+	}
+
+	@Override
+	public String toString() {
+		return "PasswordFileHandler [siteList=" + siteList + ", pwdFileName=" + pwdFileName + ", appUser=" + appUser
+				+ "]";
+	}
+
+}
