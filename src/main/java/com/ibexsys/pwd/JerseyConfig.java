a73823127dc2a84f96d2d710ae450a9bcc5a5422
Todd Johnston <toddjohnston.ibexsys@gmail.com>
2017-03-15 14:30:21 -0700

    Removed code from Jpassion

--- src/main/java/com/ibexsys/pwd/JerseyConfig.java
+++ src/main/java/com/ibexsys/pwd/JerseyConfig.java
@@ -2,7 +2,6 @@ package com.ibexsys.pwd;
 
 import javax.ws.rs.ApplicationPath;
 
-import com.ibexsys.pwd.test.CustomerResource;
 import com.ibexsys.pwd.test.PasswordUserModelRestTest;
 
 import org.glassfish.jersey.server.ResourceConfig;
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Component;
 public class JerseyConfig extends ResourceConfig {
 
 	public JerseyConfig() {
-		register(CustomerResource.class);
 		register(PasswordUserModelRestTest.class);
 	}
 
